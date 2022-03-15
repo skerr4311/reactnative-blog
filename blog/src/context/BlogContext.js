ec31f43790bbfe8a4c09fde31f7cd9aa28250991
Steve <sker078@aucklanduni.ac.nz>
2022-03-16 12:22:18 +1300

    hooked up blog list and function

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>;
+    const [blogPosts, setPlogPosts] = useState([]);
+
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}`}])
+    };
+
+    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost}}>{children}</BlogContext.Provider>;
 };
 
 export default BlogContext;
