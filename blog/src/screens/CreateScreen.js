f43c052bdbaf1e5bfd2554dcab9a2c0f3fb8ff82
Steve <sker078@aucklanduni.ac.nz>
2022-03-16 15:25:03 +1300

    the user can now enter a blog post

--- blog/src/screens/CreateScreen.js
+++ blog/src/screens/CreateScreen.js
@@ -5,6 +5,7 @@ import { Context } from '../context/BlogContext';
 const CreateScreen = () => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+    const { addBlogPost } = useContext(Context);
 
     return (
         <View>
@@ -12,7 +13,10 @@ const CreateScreen = () => {
             <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)}/>
             <Text style={styles.label}>Enter Content:</Text>
             <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)}/>
-            <Button title="Add Blog Post" />
+            <Button 
+                title="Add Blog Post"
+                onPress={() => addBlogPost(title, content)}
+            />
         </View>
     );
 };
