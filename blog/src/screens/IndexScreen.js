d37540f22100685b0f10cd98df11e7615f05b993
Steve <sker078@aucklanduni.ac.nz>
2022-03-16 14:30:59 +1300

    added delete function

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,21 +1,23 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-    const { state, addBlogPost } = useContext(Context);
+    const { state, addBlogPost, deleteBlogPost } = useContext(Context);
     return (
         <View style={styles.main}>
             <Button title="Add Post" onPress={addBlogPost}/>
             <FlatList 
                 data={state}
-                keyExtractor={blogPosts => blogPosts.title}
+                keyExtractor={blogPosts => blogPosts.id}
                 renderItem={({ item }) => {
                     return (
                         <View style={styles.row}>
                             <Text style={styles.title}>{item.title}</Text>
-                            <Feather style={styles.icon} name="trash" />
+                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}> 
+                                <Feather style={styles.icon} name="trash" />
+                            </TouchableOpacity>
                         </View>
                         )
                 }}
