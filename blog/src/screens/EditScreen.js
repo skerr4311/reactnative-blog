9225e6993224ebef96e0b46b32d6ed73f2966686
Steve <sker078@aucklanduni.ac.nz>
2022-03-16 15:56:00 +1300

    set up edit screen

--- blog/src/screens/EditScreen.js
+++ blog/src/screens/EditScreen.js
@@ -3,13 +3,17 @@ import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
 
 const EditScreen = ({ navigation }) => {
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    const { addBlogPost } = useContext(Context);
+    const { state } = useContext(Context);
+    const blogPost = state.find(
+        blogPost => blogPost.id === navigation.getParam('id')
+    );
+
+    const [title, setTitle] = useState(blogPost.title);
+    const [content, setContent] = useState(blogPost.content);
 
     return (
         <View>
-            <Text style={styles.label}>Enter Title: {navigation.getParam('id')}</Text>
+            <Text style={styles.label}>Enter Title:</Text>
             <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)}/>
             <Text style={styles.label}>Enter Content:</Text>
             <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)}/>
